78d925e45abe1df1b33819e4b9699f4d708f036d
Vasilii P <vasily.perfilyev@gmail.com>
2023-05-24 06:41:34 -0700

    Change email type to citext in postgres

--- migrations/000002_user.up.sql
+++ migrations/000002_user.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS app_user (
     user_id bigserial PRIMARY KEY,  
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    email text UNIQUE NOT NULL,
+    email citext UNIQUE NOT NULL,
     password_hash bytea NOT NULL,
     user_type_id smallint NOT NULL,
     version integer NOT NULL DEFAULT 1
