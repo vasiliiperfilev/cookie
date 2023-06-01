8b83f491f3ea32693b0b00b4e1d4e1e9c39b11c2
Vasilii P <vasily.perfilyev@gmail.com>
2023-05-31 18:19:12 -0700

    Remove old migrations

--- backend/internal/migrate/migrations/000001_create_user_type_table.down.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE user_type DROP CONSTRAINT IF EXISTS user_type_type_name_max_len_check;
-ALTER TABLE user_type DROP CONSTRAINT IF EXISTS user_type_type_name_min_len_check;
-
-DROP TABLE IF EXISTS user_type;
