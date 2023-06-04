676be5bffc98656283e4bf32d7ddf694302a6be0
Vasilii P <vasily.perfilyev@gmail.com>
2023-06-04 12:45:23 -0700

    Refactor angular auth guard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,22 +1,22 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from "./home";
-import { AuthGuard } from "./_helpers";
+import { HomeComponent } from './home';
+import { canActivate } from './_helpers';
 
 const accountModule = () =>
-  import("./account/account.module").then((x) => x.AccountModule);
+  import('./account/account.module').then((x) => x.AccountModule);
 
 const routes: Routes = [
-  { path: "", component: HomeComponent, canActivate: [AuthGuard] },
-  { path: "account", loadChildren: accountModule },
+  { path: '', component: HomeComponent, canActivate: [canActivate] },
+  { path: 'account', loadChildren: accountModule },
 
   // otherwise redirect to home
-  { path: "**", redirectTo: "" }
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
