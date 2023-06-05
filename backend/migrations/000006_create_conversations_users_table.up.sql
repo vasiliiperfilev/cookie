bff43743acd118ebd7eb2647437bbbd3fb4263f9
Vasilii P <vasily.perfilyev@gmail.com>
2023-06-05 12:55:42 -0700

    Update conversations_users migration

--- backend/migrations/000006_create_conversations_users_table.up.sql
+++ backend/migrations/000006_create_conversations_users_table.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS conversations_users (
     conversation_id bigint NOT NULL REFERENCES conversations(conversation_id) ON DELETE CASCADE,
     user_id bigint NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    last_read_message_id bigint DEFAULT 0 REFERENCES messages(message_id) ON DELETE CASCADE,
     PRIMARY KEY(conversation_id, user_id)
 );
 
