9a804487914ebe2a8f5f6b564a693f9bcc484f2c
Vasilii P <vasily.perfilyev@gmail.com>
2023-06-05 12:11:24 -0700

    Fix test fixtures

--- backend/fixtures/fixtures.sql
+++ backend/fixtures/fixtures.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (user_id, email, password_hash, user_type_id, image_id)
+INSERT INTO users (email, password_hash, user_type_id, image_id)
 VALUES 
-    (1, 'test1@user', 'hash', 1, 'test1'),
-    (2, 'test2@user', 'hash', 1, 'test2');
+    ('test1@user', 'hash', 1, 'test1'),
+    ('test2@user', 'hash', 1, 'test2');
