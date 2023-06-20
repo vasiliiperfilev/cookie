459c622ec4f7e57a8c6ac33e3b81372e8b478c88
Vasilii P <vasily.perfilyev@gmail.com>
2023-06-19 17:57:31 -0700

    Change some text typex to citext for search requests

--- backend/migrations/000002_create_users_table.up.sql
+++ backend/migrations/000002_create_users_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     user_id bigserial PRIMARY KEY,  
-    name varchar(255) NOT NULL,
+    name citext NOT NULL,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     email citext UNIQUE NOT NULL,
     password_hash bytea NOT NULL,
--- backend/migrations/000004_create_messages_table.up.sql
+++ backend/migrations/000004_create_messages_table.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS messages (
     conversation_id bigint NOT NULL, -- constrait is added when conversations relation created
     prev_message_id bigint DEFAULT 0 REFERENCES messages(message_id),
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    content text NOT NULL
+    content citext NOT NULL
 );
 
 INSERT INTO messages (message_id, sender_id, conversation_id, content)
