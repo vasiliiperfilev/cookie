218e4fe9325db36057b50cbc2913e84abc7787d1
Vasilii P <vasily.perfilyev@gmail.com>
2023-07-03 12:03:07 -0700

    Add updated_at to order table

--- backend/migrations/000010_create_orders_table.up.sql
+++ backend/migrations/000010_create_orders_table.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS orders (
     order_id bigserial PRIMARY KEY,
     messsage_id bigint REFERENCES messages(message_id),
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     supplier_comment text NOT NULL DEFAULT '',
     client_comment text NOT NULL DEFAULT ''
 );
