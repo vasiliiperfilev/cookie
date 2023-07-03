72f964e53fffa43725f275f648d5dcf86d64aa73
Vasilii P <vasily.perfilyev@gmail.com>
2023-07-03 11:59:30 -0700

    Creade user permissions migrations

---
+++ backend/migrations/000014_create_permissions_table.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS permissions(
+    permission_id smallserial PRIMARY KEY,
+    name varchar(255) NOT NULL
+);
+
+INSERT INTO permissions (name)
+VALUES 
+    ('order:create'),
+    ('order:accept'),
+    ('order:decline'),
+    ('order:fulfill'),
+    ('order:confirm_fulfillemnt'),
+    ('order:supplier_changes'),
+    ('order:client_changes');
